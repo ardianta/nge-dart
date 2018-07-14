9b67a26f1662f99512e50fa94bf8c78c5fd04c67
Dian <dian@petanikode.com>
2018-07-15 03:21:25 +0800

    ditambahkan contoh program input dan variabel

---
+++ variabel.dart
@@ -0,0 +1,15 @@
+void main(){
+    // membaut variabel dengan tipe data
+    String nama = "Dian";
+    int umur = 23;
+    double tinggi = 180.43;
+    bool isMenikah = false;
+
+    // membuat veriabel dengan kata kunci var
+    var alamat = "Lombok, Indonesia";
+
+    // mencetak variabel
+    print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+    print("Menikah: $isMenikah");
+    print("Alamat: $alamat");
+}
